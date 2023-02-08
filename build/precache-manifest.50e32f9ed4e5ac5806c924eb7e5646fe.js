7f17913e9c08ef37069a75492d03370534ab8e73
mukulpadwal <mukulpadwal786@gmail.com>
2023-02-08 15:59:34 +0530

    Changes for production

---
+++ build/precache-manifest.50e32f9ed4e5ac5806c924eb7e5646fe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "28d3058c0dd9135eee28",
+    "url": "/static/js/main.28d3058c.chunk.js"
+  },
+  {
+    "revision": "e4d0dd641036b51809bd",
+    "url": "/static/js/1.e4d0dd64.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "683d7abef769299b980a64857c0cf0a2",
+    "url": "/index.html"
+  }
+];
